1337ef7ac2140763f0572100cd699ea5c77851d1
ArmKung <s.arm.kung@gmail.com>
2016-08-10 15:48:10 +0700

    Fix revert

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 var BEGIN = 'BEGIN';
 var COMMIT = 'COMMIT';
 var REVERT = 'REVERT';
@@ -11,61 +15,73 @@ module.exports.BEGIN = BEGIN;
 module.exports.COMMIT = COMMIT;
 module.exports.REVERT = REVERT;
 function optimist(fn) {
+  var mapActionToOptimist = arguments.length <= 1 || arguments[1] === undefined ? function (action) {
+    return action.optimist;
+  } : arguments[1];
+
   function beginReducer(state, action) {
-    let {optimist, innerState} = separateState(state);
-    optimist = optimist.concat([{beforeState: innerState, action}]);
-    innerState = fn(innerState, action);
-    validateState(innerState, action);
-    return {optimist, ...innerState};
+    var _separateState = separateState(state);
+
+    var optimist = _separateState.optimist;
+    var innerState = _separateState.innerState;
+
+    if(action){
+      optimist = optimist.concat([{ beforeState: innerState, action: action }]);
+      innerState = fn(innerState, action);
+      validateState(innerState, action);
+    }
+    
+    return _extends({ optimist: optimist }, innerState);
   }
   function commitReducer(state, action) {
-    let {optimist, innerState} = separateState(state);
-    var newOptimist = [], started = false, committed = false;
+    var _separateState2 = separateState(state);
+
+    var optimist = _separateState2.optimist;
+    var innerState = _separateState2.innerState;
+
+    var optimistAction = mapActionToOptimist(action);
+    var newOptimist = [],
+        started = false,
+        committed = false;
     optimist.forEach(function (entry) {
       if (started) {
-        if (
-          entry.beforeState &&
-          matchesTransaction(entry.action, action.optimist.id)
-        ) {
+        if (entry.beforeState && matchesTransaction(entry.action, optimistAction.id)) {
           committed = true;
-          newOptimist.push({action: entry.action});
+          newOptimist.push({ action: entry.action });
         } else {
           newOptimist.push(entry);
         }
-      } else if (
-        entry.beforeState &&
-        !matchesTransaction(entry.action, action.optimist.id)
-      ) {
+      } else if (entry.beforeState && !matchesTransaction(entry.action, optimistAction.id)) {
         started = true;
         newOptimist.push(entry);
-      } else if (
-        entry.beforeState &&
-        matchesTransaction(entry.action, action.optimist.id)
-      ) {
+      } else if (entry.beforeState && matchesTransaction(entry.action, optimistAction.id)) {
         committed = true;
       }
     });
     if (!committed) {
-      console.error('Cannot commit transaction with id "' + action.optimist.id + '" because it does not exist');
+      console.error('Cannot commit transaction with id "' + optimistAction.id + '" because it does not exist');
     }
     optimist = newOptimist;
     return baseReducer(optimist, innerState, action);
   }
   function revertReducer(state, action) {
-    let {optimist, innerState} = separateState(state);
-    var newOptimist = [], started = false, gotInitialState = false, currentState = innerState;
+    var _separateState3 = separateState(state);
+
+    var optimist = _separateState3.optimist;
+    var innerState = _separateState3.innerState;
+
+    var optimistAction = mapActionToOptimist(action);
+    var newOptimist = [],
+        started = false,
+        gotInitialState = false,
+        currentState = innerState;
     optimist.forEach(function (entry) {
-      if (
-        entry.beforeState &&
-        matchesTransaction(entry.action, action.optimist.id)
-      ) {
+      if (entry.beforeState && matchesTransaction(entry.action, optimistAction.id)) {
         currentState = entry.beforeState;
         gotInitialState = true;
       }
-      if (!matchesTransaction(entry.action, action.optimist.id)) {
-        if (
-          entry.beforeState
-        ) {
+      if (!matchesTransaction(entry.action, optimistAction.id)) {
+        if (entry.beforeState) {
           started = true;
         }
         if (started) {
@@ -79,28 +95,32 @@ function optimist(fn) {
           }
         }
         if (gotInitialState) {
-          currentState = fn(currentState, entry.action);
           validateState(innerState, action);
         }
       }
     });
     if (!gotInitialState) {
-      console.error('Cannot revert transaction with id "' + action.optimist.id + '" because it does not exist');
+      console.error('Cannot revert transaction with id "' + optimistAction.id + '" because it does not exist');
     }
     optimist = newOptimist;
     return baseReducer(optimist, currentState, action);
   }
   function baseReducer(optimist, innerState, action) {
     if (optimist.length) {
-      optimist = optimist.concat([{action}]);
+      optimist = optimist.concat([{ action: action }]);
     }
     innerState = fn(innerState, action);
     validateState(innerState, action);
-    return {optimist, ...innerState};
+    return _extends({ optimist: optimist }, innerState);
+  }
+  function matchesTransaction(action, id) {
+    var optimistAction = mapActionToOptimist(action);
+    return optimistAction && optimistAction.id === id;
   }
   return function (state, action) {
-    if (action.optimist) {
-      switch (action.optimist.type) {
+    var optimistAction = mapActionToOptimist(action);
+    if (optimistAction) {
+      switch (optimistAction.type) {
         case BEGIN:
           return beginReducer(state, action);
         case COMMIT:
@@ -109,33 +129,27 @@ function optimist(fn) {
           return revertReducer(state, action);
       }
     }
-    let separated = separateState(state);
+    var separated = separateState(state);
     return baseReducer(separated.optimist, separated.innerState, action);
   };
 }
 
-function matchesTransaction(action, id) {
-  return (
-    action.optimist &&
-    action.optimist.id === id
-  );
-}
-
 function validateState(newState, action) {
   if (!newState || typeof newState !== 'object' || Array.isArray(newState)) {
-    throw new TypeError(
-      'Error while handling "' +
-      action.type +
-      '": Optimist requires that state is always a plain object.'
-    );
+    throw new TypeError('Error while handling "' + action.type + '": Optimist requires that state is always a plain object.');
   }
 }
 
 function separateState(state) {
   if (!state) {
-    return {optimist: INITIAL_OPTIMIST, innerState: state};
+    return { optimist: INITIAL_OPTIMIST, innerState: state };
   } else {
-    let {optimist = INITIAL_OPTIMIST, ...innerState} = state;
-    return {optimist, innerState};
+    var _state$optimist = state.optimist;
+
+    var _optimist = _state$optimist === undefined ? INITIAL_OPTIMIST : _state$optimist;
+
+    var innerState = _objectWithoutProperties(state, ['optimist']);
+
+    return { optimist: _optimist, innerState: innerState };
   }
 }
